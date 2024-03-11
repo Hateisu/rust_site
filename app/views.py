2f32dd3236bfe2612541298209b7d22a092b8bc9
lappland <timokyr2007@gmail.com>
2024-03-12 00:30:42 +0500

    login rework

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .models import *
 # Create your views here.
 
@@ -30,11 +31,30 @@ def login_user(request):
         return redirect("index")
     else:
         if request.method == "POST":
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request,username=username, password=password)
-            print(user)
-            if user is not None:
-                login(request, user)
-                return redirect("index")
+            match request.POST['action']:
+                case "registration":
+                    username = request.POST['username']
+                    password = request.POST['password']
+                    password_confirm = request.POST['confirm_password']
+                    if password==password_confirm:
+                        try:
+                            user = User.objects.create_user(username=username,
+                                 password=password)
+                            user_l = authenticate(request,username=username, password=password)
+                            if user_l is not None:
+                                login(request, user_l)
+                                return redirect("index")
+                        except Exception as e:
+                            pass
+                        
+                case "login":
+                    username = request.POST['username']
+                    password = request.POST['password']
+                    user = authenticate(request,username=username, password=password)
+                    print(user)
+                    if user is not None:
+                        login(request, user)
+                        return redirect("index")
+                case _:
+                    pass
         return render(request=request, template_name="avto.html", context=context)
